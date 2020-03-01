91c52b94ca64c893acd0cf5ee25321c33c1ba6cb
Thomas Kolarik <tokolarik@gmail.com>
2020-03-01 18:29:21 -0500

    Create shared memory tests.

--- module5/assignment.cu
+++ module5/assignment.cu
@@ -4,8 +4,7 @@
 #include <chrono>
 #include <stdio.h>
 
-static const int CYPHER_OFFSET = 3;
-
+// GPU add c[i] = a[i] + b[i]
 __global__ void add(int * a, int * b, int * c)
 {
     const unsigned int thread_idx = (blockIdx.x * blockDim.x) + threadIdx.x;
@@ -13,6 +12,7 @@ __global__ void add(int * a, int * b, int * c)
     c[thread_idx] = a[thread_idx] + b[thread_idx];
 }
 
+// GPU subtract c[i] = a[i] - b[i]
 __global__ void subtract(int * a, int * b, int * c)
 {
     const unsigned int thread_idx = (blockIdx.x * blockDim.x) + threadIdx.x;
@@ -20,6 +20,7 @@ __global__ void subtract(int * a, int * b, int * c)
     c[thread_idx] = a[thread_idx] - b[thread_idx];
 }
 
+// GPU multiply c[i] = a[i] * b[i]
 __global__ void mult(int * a, int * b, int * c)
 {
     const unsigned int thread_idx = (blockIdx.x * blockDim.x) + threadIdx.x;
@@ -27,6 +28,15 @@ __global__ void mult(int * a, int * b, int * c)
     c[thread_idx] = a[thread_idx] * b[thread_idx];
 }
 
+// GPU div c[i] = a[i] / b[i]
+__global__ void div(int *a, int * b, int * c)
+{
+    const unsigned int thread_idx = (blockIdx.x * blockDim.x) + threadIdx.x;
+    
+    c[thread_idx] = a[thread_idx] / b[thread_idx];
+}
+
+// GPU mod c[i] = a[i] % b[i]
 __global__ void mod(int * a, int * b, int * c)
 {
     const unsigned int thread_idx = (blockIdx.x * blockDim.x) + threadIdx.x;
@@ -34,24 +44,53 @@ __global__ void mod(int * a, int * b, int * c)
     c[thread_idx] = a[thread_idx] % b[thread_idx];
 }
 
-__global__ void caesarCypher(char * textToEncrypt, const int offset)
+// Copies data from one array to another
+__device__ void copyData(const int * const srcArr,
+									u32 * const destArr,
+									const u32 tid,
+									const u32 size,
+									const u32 tid)
 {
-    const unsigned int thread_idx = (blockIdx.x * blockDim.x) + threadIdx.x;
-    char c;
-    c = textToEncrypt[thread_idx] - offset;
-    
-    // This assume the input array is all capital letters
-    if (c < 'A')
-    {
-        c += 'Z' - 'A' + 1;
-    }
-    else if (c > 'Z')
-    {
-        c -= 'Z' - 'A' + 1;
-    }
-    textToEncrypt[thread_idx] = c;
+	// Copy data into temp store
+	for(u32 i = 0; i<num_elements; i++)
+	{
+		destArr[i+tid] = srcArr[i+tid];
+	}
+	__syncthreads();
 }
 
+__global__ void executeSharedMathOperations(int * a, int * b, int * addDest, int * subDest, int * multDest, int * divDest, int * modDest, int size)
+{
+	const u32 tid = (blockIdx.x * blockDim.x) + threadIdx.x;
+    __shared__ sharedA[size];
+    __shared__ sharedB[size];
+    __shared__ sharedRet[size];
+    
+    copyData(a, sharedA, tid, size);
+    copyData(b, sharedB, tid, size);
+    
+    // Add sharedA to sharedB and store in addDest
+    add(sharedA, sharedB, sharedRet);
+    copyData(sharedRet, addDest, tid, size);
+    
+    // Subtract sharedB from sharedA and store in subDest
+    sub(sharedA, sharedB, sharedRet);
+    copyData(sharedRet, subDest, tid, size);
+    
+    // Multiply sharedA to sharedB and store in mutlDest
+    mult(sharedA, sharedB, sharedRet);
+    copyData(sharedRet, multDest, tid, size);
+    
+    // Divide sharedA by sharedB and store in divDest
+    div(sharedA, sharedB, sharedRet);
+    copyData(sharedRet, divDest, tid, size);
+    
+    // Mod sharedA by sharedB and store in modDest
+    mod(sharedA, sharedB, sharedRet);
+    copyData(sharedRet, modDest, tid, size);
+}
+
+// Host (Cpu) add c[i] = a[i] + b[i]
 void hostAdd(int * a, int * b, int *c, const int size)
 {
     for (int i = 0; i < size; ++i)
@@ -60,6 +99,7 @@ void hostAdd(int * a, int * b, int *c, const int size)
     }
 }
 
+// Host (Cpu) sub c[i] = a[i] - b[i]
 void hostSub(int * a, int * b, int *c, const int size)
 {
     for (int i = 0; i < size; ++i)
@@ -68,6 +108,7 @@ void hostSub(int * a, int * b, int *c, const int size)
     }
 }
 
+// Host (Cpu) multiply c[i] = a[i] * b[i]
 void hostMult(int * a, int * b, int *c, const int size)
 {
     for (int i = 0; i < size; ++i)
@@ -76,6 +117,23 @@ void hostMult(int * a, int * b, int *c, const int size)
     }
 }
 
+// Host (Cpu) divide c[i] = a[i] / b[i]
+void hostDiv(int * a, int * b, int *c, const int size)
+{
+    for (int i = 0; i < size; ++i)
+    {
+        if (b[i] != 0)
+        {
+            c[i] = a[i] / b[i];
+        }
+        else
+        {
+            c[i] = 0;
+        }
+    }
+}
+
+// Host (Cpu) mod c[i] = a[i] % b[i]
 void hostMod(int * a, int * b, int *c, const int size)
 {
     for (int i = 0; i < size; ++i)
@@ -93,27 +151,8 @@ void hostMod(int * a, int * b, int *c, const int size)
     }
 }
 
-void hostCaesarCypher(char * textToEncrypt, const int offset, const int size)
-{
-    
-    for (int i = 0; i < size; ++i)
-    {
-        char c;
-        c = textToEncrypt[i] - offset;
-        
-        // This assume the input array is all capital letters
-        if (c < 'A')
-        {
-            c += 'Z' - 'A' + 1;
-        }
-        else if (c > 'Z')
-        {
-            c -= 'Z' - 'A' + 1;
-        }
-        textToEncrypt[i] = c;
-    }
-}
-
+// Executes each of the host (cpu) tests by creating local memory and executing all 5 math operations on the data.
+// The data is filled with random numbers that uses the same seed as the GPU tests.
 void executeHostTest(const int totalThreads, const int blockSize, const int numBlocks)
 {
     int a[totalThreads], b[totalThreads], c[totalThreads];
@@ -139,25 +178,64 @@ void executeHostTest(const int totalThreads, const int blockSize, const int numB
     // Multiply all of the numbers c[i] = a[i] * b[i];
     hostMult(a,b,c, totalThreads);
     
+    // Divides all of the numbers c[i] = a[i] / b[i]; if b[i] == 0, c[i] = 0
+    hostDiv(a,b,c, totalThreads);
+    
     // Mod all of the numbers c[i] = a[i] % b[i];
     hostMod(a,b,c, totalThreads);
-    
-    // Allocate space for a character array.
-    int minChar = 'A';
-    int maxChar = 'Z';
-    std::uniform_int_distribution<int> charDist(minChar, maxChar);
-    
-    char textToEncrypt[totalThreads];
-    
-    for (int i = 0; i < totalThreads; ++i)
-    {
-        textToEncrypt[i] = charDist(gen);
-    }
-    
-    hostCaesarCypher(textToEncrypt, totalThreads, CYPHER_OFFSET);
 }
 
-void executeGPUTest(const int totalThreads, const int blockSize, const int numBlocks)
+// Executes each of the global memory gpu tests by creating local memory, copying it global memory, and then performing
+//  all 5 math operations on the data. The data is filled with random numbers that uses the same seed as the CPU tests.
+void executeGlobalTest(const int totalThreads, const int blockSize, const int numBlocks)
+{
+    int a[totalThreads], b[totalThreads], add_dest[totalThreads], sub_dest[totalThreads], mult_dest[totalThreads], div_dest[totalThreads], mod_dest[totalThreads];
+    
+    int *gpu_a, *gpu_b, *gpu_add_dest, *gpu_sub_dest, *gpu_mult_dest, *gpu_div_dest, *gpu_mod_dest;
+
+    cudaMalloc((void**)&gpu_a,         totalThreads * sizeof(int));
+    cudaMalloc((void**)&gpu_b,         totalThreads * sizeof(int));
+    cudaMalloc((void**)&gpu_add_dest,  totalThreads * sizeof(int));
+    cudaMalloc((void**)&gpu_sub_dest,  totalThreads * sizeof(int));
+    cudaMalloc((void**)&gpu_mult_dest, totalThreads * sizeof(int));
+    cudaMalloc((void**)&gpu_div_dest,  totalThreads * sizeof(int));
+    cudaMalloc((void**)&gpu_mod_dest,  totalThreads * sizeof(int));
+    
+    // Create a random generate that will generate random numbers from 0 to 4.
+    // Use a set seed so output is deterministic
+    unsigned seed = 12345;
+    std::default_random_engine gen(seed);
+    std::uniform_int_distribution<int> dist(0,4);
+    
+    for (size_t i = 0; i < totalThreads; ++i)
+    {
+        a[i] = i;
+        b[i] = dist(gen);
+    }
+    
+    cudaMemcpy(gpu_a, a, totalThreads * sizeof(int), cudaMemcpyHostToDevice);
+    cudaMemcpy(gpu_b, b, totalThreads * sizeof(int), cudaMemcpyHostToDevice);
+    
+    executeSharedMathOperations<<<numBlocks, blockSize>>>(gpu_a,gpu_b,gpu_add_dest,gpu_sub_dest,gpu_mult_dest,gpu_div_dest,gpu_mod_dest);
+    
+    cudaMemcpy(add_dest, gpu_add_dest,   totalThreads*sizeof(int), cudaMemcpyDeviceToHost);
+    cudaMemcpy(sub_dest, gpu_sub_dest,   totalThreads*sizeof(int), cudaMemcpyDeviceToHost);    
+    cudaMemcpy(mult_dest, gpu_mult_dest, totalThreads*sizeof(int), cudaMemcpyDeviceToHost);
+    cudaMemcpy(div_dest, gpu_div_dest,   totalThreads*sizeof(int), cudaMemcpyDeviceToHost);
+    cudaMemcpy(mod_dest, gpu_mod_dest,   totalThreads*sizeof(int), cudaMemcpyDeviceToHost);
+    
+    cudaFree(gpu_a);
+    cudaFree(gpu_b);
+    cudaFree(gpu_add_dest);
+    cudaFree(gpu_sub_dest);
+    cudaFree(gpu_mult_dest);
+    cudaFree(gpu_div_dest);
+    cudaFree(gpu_mod_dest);
+}
+
+// Executes each of the shared memory gpu tests by creating local memory, copying it global memory, and then performing
+// all 5 math operations on the data. The data is filled with random numbers that uses the same seed as the CPU tests.
+void executeSharedTest(const int totalThreads, const int blockSize, const int numBlocks)
 {
     int a[totalThreads], b[totalThreads], c[totalThreads];
     
@@ -197,6 +275,11 @@ void executeGPUTest(const int totalThreads, const int blockSize, const int numBl
     
     cudaMemcpy(c, gpu_c, totalThreads*sizeof(int), cudaMemcpyDeviceToHost);
     
+    // Divide all of the numbers c[i] = a[i] / b[i];
+    div<<<numBlocks, blockSize>>>(gpu_a,gpu_b,gpu_c);
+    
+    cudaMemcpy(c, gpu_c, totalThreads*sizeof(int), cudaMemcpyDeviceToHost);
+    
     // Mod all of the numbers c[i] = a[i] % b[i];
     mod<<<numBlocks, blockSize>>>(gpu_a,gpu_b,gpu_c);
     
@@ -204,31 +287,7 @@ void executeGPUTest(const int totalThreads, const int blockSize, const int numBl
     
     cudaFree(gpu_a);
     cudaFree(gpu_b);
-    cudaFree(gpu_c);
-    
-    // Allocate space for a character array.
-    int minChar = 'A';
-    int maxChar = 'Z';
-    std::uniform_int_distribution<int> charDist(minChar, maxChar);
-    
-    char textToEncrypt[totalThreads];
-    
-    for (int i = 0; i < totalThreads; ++i)
-    {
-        textToEncrypt[i] = charDist(gen);
-    } 
-    
-    char * gpuTextToEncrypt;
-    cudaMalloc((void**)&gpuTextToEncrypt, totalThreads * sizeof(char));
-    cudaMemcpy(gpuTextToEncrypt, a, totalThreads * sizeof(char), cudaMemcpyHostToDevice);
-    
-    caesarCypher<<<numBlocks, blockSize>>>(gpuTextToEncrypt, CYPHER_OFFSET);
-    
-    cudaMemcpy(textToEncrypt, gpuTextToEncrypt, totalThreads*sizeof(int), cudaMemcpyDeviceToHost);
-    
-    
-    cudaFree(textToEncrypt);
-    
+    cudaFree(gpu_c);    
 }
 
 void printArray(const int * const arr, const int xSize, const int ySize)
@@ -276,10 +335,16 @@ int main(int argc, char** argv)
     std::cout << "Host execution took: " << totalTime.count() << " seconds." << std::endl;
     
     startTime = std::chrono::system_clock::now();
-    executeGPUTest(totalThreads, blockSize, numBlocks);
+    executeGlobalTest(totalThreads, blockSize, numBlocks);
     endTime = std::chrono::system_clock::now();
     totalTime = endTime-startTime;
-    std::cout << "GPU execution took: " << totalTime.count() << " seconds." << std::endl;
+    std::cout << "Global Memory execution took: " << totalTime.count() << " seconds." << std::endl;
+    
+    startTime = std::chrono::system_clock::now();
+    executeSharedTest(totalThreads, blockSize, numBlocks);
+    endTime = std::chrono::system_clock::now();
+    totalTime = endTime-startTime;
+    std::cout << "Shared Memory execution took: " << totalTime.count() << " seconds." << std::endl;
     
     return 0;
 }
