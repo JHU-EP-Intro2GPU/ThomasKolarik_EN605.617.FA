956a8f2f7bb0cbf6b685684eb95b30762f979d3e
Thomas Kolarik <tokolarik@gmail.com>
2020-03-08 18:51:42 -0400

    Fix Typo

--- module6/assignment.cu
+++ module6/assignment.cu
@@ -16,7 +16,7 @@ enum gpu_tests_enum
     NUM_GPU_TESTS
 };
 
-gpu_tests_enum& opeartor++(gpu_tests_enum& e)
+gpu_tests_enum& operator++(gpu_tests_enum& e)
 {
     return e = (e == REGISTER) ? GLOBAL : static_cast<gpu_tests_enum>(static_cast<int>(e)+1);
 }
