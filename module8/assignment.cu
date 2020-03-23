674c92d7001ed47847d0d271dd0923e3767a9e90
Thomas Kolarik <tokolarik@gmail.com>
2020-03-23 14:53:28 -0400

    Remove a lot of left over code

--- module8/assignment.cu
+++ module8/assignment.cu
@@ -1,3 +1,4 @@
+#include <cublas.h>
 #include <curand.h>
 #include <curand_kernel.h>
 #include <iostream>
@@ -8,8 +9,8 @@
 /* this GPU kernel function is used to initialize the random states */
 __global__ void initCudaRandom(unsigned int seed, curandState_t* states)
 {
-  const unsigned int thread_idx = (blockIdx.x * blockDim.x) + threadIdx.x;
-  curand_init(seed,
+    const unsigned int thread_idx = (blockIdx.x * blockDim.x) + threadIdx.x;
+    curand_init(seed,
               thread_idx,
               0,
               &states[blockIdx.x]);
@@ -17,9 +18,10 @@ __global__ void initCudaRandom(unsigned int seed, curandState_t* states)
 
 __global__ void populateCudaRandom(curandState_t* state, int* result)
 {
-  const unsigned int thread_idx = (blockIdx.x * blockDim.x) + threadIdx.x;
+    const int MAX_RAND = 100;
+    const unsigned int thread_idx = (blockIdx.x * blockDim.x) + threadIdx.x;
 
-  result[thread_idx] = curand(&state[thread_idx]) % MAX;
+    result[thread_idx] = curand(&state[thread_idx]) % MAX_RAND;
 }
     
 // Executes matrix multiplication using the CUDA BLAS library.
@@ -30,9 +32,9 @@ void executeMultTest(const int totalThreads, const int blockSize, const int numB
     const unsigned int seed = 1;
     
     // Allocate space for all of the device Matricies
-    float *hostA = (float*)malloc(totalThreads*sizeof(float));
-    float *hostB = (float*)malloc(totalThreads*sizeof(float));
-    float *hostC = (float*)malloc(totalThreads*sizeof(float));
+    int *hostA = (int*)malloc(totalThreads*sizeof(int));
+    int *hostB = (int*)malloc(totalThreads*sizeof(int));
+    int *hostC = (int*)malloc(totalThreads*sizeof(int));
     
     cublasStatus status;
     cublasInit();
@@ -40,50 +42,29 @@ void executeMultTest(const int totalThreads, const int blockSize, const int numB
     // Initialize random number generation
     curandState_t* states;
     cudaMalloc((void**) &states, totalThreads * sizeof(curandState_t));
-    init<<<numBlocks, blockSize>>>(seed, states);
+    initCudaRandom<<<numBlocks, blockSize>>>(seed, states);
 
     // Allocate space for all of the device matricies
-    float *gpuA = (float*)cublasAlloc(totalThreads*sizeof(float));
-    float *gpuB = (float*)cublasAlloc(totalThreads*sizeof(float));
-    float *gpuC = (float*)cublasAlloc(blockSize*blockSize*sizeof(float));
+    int *gpuA = (int*)cublasAlloc(totalThreads*sizeof(int));
+    int *gpuB = (int*)cublasAlloc(totalThreads*sizeof(int));
+    int *gpuC = (int*)cublasAlloc(blockSize*blockSize*sizeof(int));
 
     /* invoke the kernel to get some random numbers */
     populateCudaRandom<<<numBlocks, blockSize>>>(states, gpuA);
     populateCudaRandom<<<blockSize, numBlocks>>>(states, gpuB);
     populateCudaRandom<<<blockSize, blockSize>>>(states, gpuC);
 
-    float* AA; float* BB; float* CC;
-
-    /*ALLOCATE ON THE DEVICE*/
-    status=cublasAlloc(HA*WA,sizeof(float),(void**)&AA);
-    if (status != CUBLAS_STATUS_SUCCESS) {
-      fprintf (stderr, "!!!! device memory allocation error (A)\n");
-      return EXIT_FAILURE;
-    }
-
-    status=cublasAlloc(HB*WB,sizeof(float),(void**)&BB);
-    if (status != CUBLAS_STATUS_SUCCESS) {
-      fprintf (stderr, "!!!! device memory allocation error (A)\n");
-      return EXIT_FAILURE;
-    }
-
-    status=cublasAlloc(HC*WC,sizeof(float),(void**)&CC);
-    if (status != CUBLAS_STATUS_SUCCESS) {
-      fprintf (stderr, "!!!! device memory allocation error (A)\n");
-      return EXIT_FAILURE;
-    }
-
     /*SET MATRIX*/
     status=cublasSetMatrix(numBlocks,blockSize,sizeof(float),hostA,numBlocks,gpuA,numBlocks);
     if (status != CUBLAS_STATUS_SUCCESS) {
       fprintf (stderr, "!!!! device memory allocation error (A)\n");
-      return EXIT_FAILURE;
+      return;
     }
 
     status=cublasSetMatrix(blockSize,numBlocks,sizeof(float),hostB,blockSize,gpuB,blockSize);
     if (status != CUBLAS_STATUS_SUCCESS) {
       fprintf (stderr, "!!!! device memory allocation error (A)\n");
-      return EXIT_FAILURE;
+      return;
     }
 
     /*KERNEL*/
@@ -92,12 +73,12 @@ void executeMultTest(const int totalThreads, const int blockSize, const int numB
     status = cublasGetError();
     if (status != CUBLAS_STATUS_SUCCESS) {
       fprintf (stderr, "!!!! kernel execution error.\n");
-      return EXIT_FAILURE;
+      return;
     }
     cublasGetMatrix(numBlocks,blockSize,sizeof(float),gpuC,blockSize,hostC,blockSize);
     if (status != CUBLAS_STATUS_SUCCESS) {
       fprintf (stderr, "!!!! device read error (A)\n");
-      return EXIT_FAILURE;
+      return;
     }
     
     // Free all memory allocations
@@ -108,39 +89,19 @@ void executeMultTest(const int totalThreads, const int blockSize, const int numB
     status = cublasFree(gpuA);
     if (status != CUBLAS_STATUS_SUCCESS) {
       fprintf (stderr, "!!!! memory free error (A)\n");
-      return EXIT_FAILURE;
+      return;
     }
     status = cublasFree(gpuB);
     if (status != CUBLAS_STATUS_SUCCESS) {
       fprintf (stderr, "!!!! memory free error (B)\n");
-      return EXIT_FAILURE;
+      return;
     }
     status = cublasFree(gpuC);
     if (status != CUBLAS_STATUS_SUCCESS) {
       fprintf (stderr, "!!!! memory free error (C)\n");
-      return EXIT_FAILURE;
+      return;
     }
 }
-
-// Executes the given test type 
-// Data is filled with random numbers.
-void executeGPUTest(const int totalThreads, const int blockSize, const int numBlocks, const gpu_tests_enum testType)
-{    
-    switch(testType)
-    {
-        case BLAS:
-            // Linear Algrebra Multiply Operations
-            executeGlobalMathOperations<<<numBlocks, blockSize>>>(gpu_a,gpu_b,gpu_add_dest,gpu_sub_dest,gpu_mult_dest,gpu_div_dest,gpu_mod_dest, numBlocks * blockSize);
-            break;
-        case SOLVE:
-            // Executes a matrix solver
-            executeSharedMathOperations<<<numBlocks, blockSize, 3 * totalThreads * sizeof(int)>>>(gpu_a,gpu_b,gpu_add_dest,gpu_sub_dest,gpu_mult_dest,gpu_div_dest,gpu_mod_dest, totalThreads);
-            break;
-        default:
-            std::cout << "Unknown test type " << testType << "!" << std::endl;
-            break;
-    }
- }
 
 // Prints an array. Used for debugging
 void printArray(const int * const arr, const int xSize, const int ySize)
@@ -182,7 +143,7 @@ int main(int argc, char** argv)
     }
 
     auto startTime = std::chrono::system_clock::now();
-    executeGPUTest(totalThreads, blockSize, numBlocks, testType);
+    executeMultTest(totalThreads, blockSize, numBlocks);
     auto endTime = std::chrono::system_clock::now();
     std::chrono::duration<double> totalTime = endTime-startTime;
     std::cout << "Execution took: " << totalTime.count() << " seconds." << std::endl;    
