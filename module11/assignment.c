ae418449d7e634b4924be2d0a72ecd9e0ce4aaf9
Thomas Kolarik <tokolarik@gmail.com>
2020-04-20 17:00:34 -0400

    Modify assignment.c...

--- module11/assignment.c
+++ module11/assignment.c
@@ -1,27 +1,327 @@
-//Based on the work of Andrew Krepps
-#include <stdio.h>
+//
+// Book:      OpenCL(R) Programming Guide
+// Authors:   Aaftab Munshi, Benedict Gaster, Timothy Mattson, James Fung, Dan Ginsburg
+// ISBN-10:   0-321-74964-2
+// ISBN-13:   978-0-321-74964-2
+// Publisher: Addison-Wesley Professional
+// URLs:      http://safari.informit.com/9780132488006/
+//            http://www.openclprogrammingguide.com
+//
 
+
+// Convolution.cpp
+//
+//    This is a simple example that demonstrates OpenCL platform, device, and context
+//    use.
+
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <string>
+#include <chrono>
+#include <random>
+
+#ifdef __APPLE__
+#include <OpenCL/cl.h>
+#else
+#include <CL/cl.h>
+#endif
+
+#if !defined(CL_CALLBACK)
+#define CL_CALLBACK
+#endif
+
+// Constants
+const unsigned int inputSignalWidth  = 49;
+const unsigned int inputSignalHeight = 49;
+
+cl_uint inputSignal[inputSignalHeight][inputSignalWidth];
+
+const unsigned int outputSignalWidth  = 43;
+const unsigned int outputSignalHeight = 43;
+
+cl_uint outputSignal[outputSignalHeight][outputSignalWidth];
+
+const unsigned int maskWidth  = 7;
+const unsigned int maskHeight = 7;
+
+cl_uint mask[maskHeight][maskWidth] =
+{
+	{0.50, 0.50, 0.50, 0.50, 0.50, 0.50, 0.50},
+    {0.50, 0.75, 0.75, 0.75, 0.75, 0.75, 0.50},
+    {0.50, 0.75, 1.00, 1.00, 1.00, 0.75, 0.50},
+    {0.50, 0.75, 1.00, 0.00, 1.00, 0.75, 0.50},
+    {0.50, 0.75, 1.00, 1.00, 1.00, 0.75, 0.50},
+    {0.50, 0.75, 0.75, 0.75, 0.75, 0.75, 0.50},
+    {0.50, 0.50, 0.50, 0.50, 0.50, 0.50, 0.50}
+};
+
+///
+// Function to check and handle OpenCL errors
+inline void 
+checkErr(cl_int err, const char * name)
+{
+    if (err != CL_SUCCESS) {
+        std::cerr << "ERROR: " <<  name << " (" << err << ")" << std::endl;
+        exit(EXIT_FAILURE);
+    }
+}
+
+void CL_CALLBACK contextCallback(
+	const char * errInfo,
+	const void * private_info,
+	size_t cb,
+	void * user_data)
+{
+	std::cout << "Error occured during context use: " << errInfo << std::endl;
+	// should really perform any clearup and so on at this point
+	// but for simplicitly just exit.
+	exit(1);
+}
+
+///
+//	main() for Convoloution example
+//
 int main(int argc, char** argv)
 {
-	// read command line arguments
-	int totalThreads = (1 << 20);
-	int blockSize = 256;
-	
-	if (argc >= 2) {
-		totalThreads = atoi(argv[1]);
-	}
-	if (argc >= 3) {
-		blockSize = atoi(argv[2]);
+    auto progStartTime = std::chrono::system_clock::now();
+    cl_int errNum;
+    cl_uint numPlatforms;
+	cl_uint numDevices;
+    cl_platform_id * platformIDs;
+	cl_device_id * deviceIDs;
+    cl_context context = NULL;
+	cl_command_queue queue;
+	cl_program program;
+	cl_kernel kernel;
+	cl_mem inputSignalBuffer;
+	cl_mem outputSignalBuffer;
+	cl_mem maskBuffer;
+
+    // First, select an OpenCL platform to run on.  
+	errNum = clGetPlatformIDs(0, NULL, &numPlatforms);
+	checkErr( 
+		(errNum != CL_SUCCESS) ? errNum : (numPlatforms <= 0 ? -1 : CL_SUCCESS), 
+		"clGetPlatformIDs"); 
+ 
+	platformIDs = (cl_platform_id *)alloca(
+       		sizeof(cl_platform_id) * numPlatforms);
+
+    errNum = clGetPlatformIDs(numPlatforms, platformIDs, NULL);
+    checkErr( 
+	   (errNum != CL_SUCCESS) ? errNum : (numPlatforms <= 0 ? -1 : CL_SUCCESS), 
+	   "clGetPlatformIDs");
+
+	// Iterate through the list of platforms until we find one that supports
+	// a CPU device, otherwise fail with an error.
+	deviceIDs = NULL;
+	cl_uint i;
+	for (i = 0; i < numPlatforms; i++)
+	{
+		errNum = clGetDeviceIDs(
+            platformIDs[i], 
+            CL_DEVICE_TYPE_GPU, 
+            0,
+            NULL,
+            &numDevices);
+		if (errNum != CL_SUCCESS && errNum != CL_DEVICE_NOT_FOUND)
+	    {
+			checkErr(errNum, "clGetDeviceIDs");
+        }
+	    else if (numDevices > 0) 
+	    {
+		   	deviceIDs = (cl_device_id *)alloca(sizeof(cl_device_id) * numDevices);
+			errNum = clGetDeviceIDs(
+				platformIDs[i],
+				CL_DEVICE_TYPE_GPU,
+				numDevices, 
+				&deviceIDs[0], 
+				NULL);
+			checkErr(errNum, "clGetDeviceIDs");
+			break;
+	   }
 	}
 
-	int numBlocks = totalThreads/blockSize;
+	// Check to see if we found at least one CPU device, otherwise return
+// 	if (deviceIDs == NULL) {
+// 		std::cout << "No CPU device found" << std::endl;
+// 		exit(-1);
+// 	}
 
-	// validate command line arguments
-	if (totalThreads % blockSize != 0) {
-		++numBlocks;
-		totalThreads = numBlocks*blockSize;
-		
-		printf("Warning: Total thread count is not evenly divisible by the block size\n");
-		printf("The total number of threads will be rounded up to %d\n", totalThreads);
+    // Next, create an OpenCL context on the selected platform.  
+    cl_context_properties contextProperties[] =
+    {
+        CL_CONTEXT_PLATFORM,
+        (cl_context_properties)platformIDs[i],
+        0
+    };
+    context = clCreateContext(
+		contextProperties, 
+		numDevices,
+        deviceIDs, 
+		&contextCallback,
+		NULL, 
+		&errNum);
+	checkErr(errNum, "clCreateContext");
+
+	std::ifstream srcFile("Convolution.cl");
+    checkErr(srcFile.is_open() ? CL_SUCCESS : -1, "reading Convolution.cl");
+
+	std::string srcProg(
+        std::istreambuf_iterator<char>(srcFile),
+        (std::istreambuf_iterator<char>()));
+
+	const char * src = srcProg.c_str();
+	size_t length = srcProg.length();
+
+	// Create program from source
+	program = clCreateProgramWithSource(
+		context, 
+		1, 
+		&src, 
+		&length, 
+		&errNum);
+	checkErr(errNum, "clCreateProgramWithSource");
+
+	// Build program
+	errNum = clBuildProgram(
+		program,
+		numDevices,
+		deviceIDs,
+		NULL,
+		NULL,
+		NULL);
+    if (errNum != CL_SUCCESS)
+    {
+        // Determine the reason for the error
+        char buildLog[16384];
+        clGetProgramBuildInfo(
+			program, 
+			deviceIDs[0], 
+			CL_PROGRAM_BUILD_LOG,
+            sizeof(buildLog), 
+			buildLog, 
+			NULL);
+
+        std::cerr << "Error in kernel: " << std::endl;
+        std::cerr << buildLog;
+		checkErr(errNum, "clBuildProgram");
+    }
+
+	// Create kernel object
+	kernel = clCreateKernel(
+		program,
+		"convolve",
+		&errNum);
+	checkErr(errNum, "clCreateKernel");
+    
+    // Create the initial signal using a random seed supplied by the user
+    // or default to 12345.
+    unsigned seed = 12345;
+    
+    if (argc >= 2) {
+        seed = atoi(argv[1]);
+    }
+    
+    std::default_random_engine gen(seed);
+    std::uniform_real_distribution<float> dist(0,10);
+    
+    for (unsigned int i = 0; i < inputSignalHeight; ++i)
+    {
+        for (unsigned int j = 0; j < inputSignalHeight; ++j)
+        {
+            inputSignal[i][j] = dist(gen);
+        }
+    }
+
+	// Now allocate buffers
+	inputSignalBuffer = clCreateBuffer(
+		context,
+		CL_MEM_READ_ONLY | CL_MEM_COPY_HOST_PTR,
+		sizeof(cl_uint) * inputSignalHeight * inputSignalWidth,
+		static_cast<void *>(inputSignal),
+		&errNum);
+	checkErr(errNum, "clCreateBuffer(inputSignal)");
+
+	maskBuffer = clCreateBuffer(
+		context,
+		CL_MEM_READ_ONLY | CL_MEM_COPY_HOST_PTR,
+		sizeof(cl_uint) * maskHeight * maskWidth,
+		static_cast<void *>(mask),
+		&errNum);
+	checkErr(errNum, "clCreateBuffer(mask)");
+
+	outputSignalBuffer = clCreateBuffer(
+		context,
+		CL_MEM_WRITE_ONLY,
+		sizeof(cl_uint) * outputSignalHeight * outputSignalWidth,
+		NULL,
+		&errNum);
+	checkErr(errNum, "clCreateBuffer(outputSignal)");
+
+	// Pick the first device and create command queue.
+	queue = clCreateCommandQueue(
+		context,
+		deviceIDs[0],
+		0,
+		&errNum);
+	checkErr(errNum, "clCreateCommandQueue");
+
+    errNum  = clSetKernelArg(kernel, 0, sizeof(cl_mem), &inputSignalBuffer);
+	errNum |= clSetKernelArg(kernel, 1, sizeof(cl_mem), &maskBuffer);
+    errNum |= clSetKernelArg(kernel, 2, sizeof(cl_mem), &outputSignalBuffer);
+	errNum |= clSetKernelArg(kernel, 3, sizeof(cl_uint), &inputSignalWidth);
+	errNum |= clSetKernelArg(kernel, 4, sizeof(cl_uint), &maskWidth);
+	checkErr(errNum, "clSetKernelArg");
+
+	const size_t globalWorkSize[2] = { outputSignalWidth, outputSignalHeight };
+    const size_t localWorkSize[2]  = { 1, 1 };
+
+    // Queue the kernel up for execution across the array
+    auto convStartTime = std::chrono::system_clock::now();
+    
+    errNum = clEnqueueNDRangeKernel(
+		queue, 
+		kernel, 
+		2,
+		NULL,
+        globalWorkSize, 
+		localWorkSize,
+        0, 
+		NULL, 
+		NULL);
+	checkErr(errNum, "clEnqueueNDRangeKernel");
+    
+	errNum = clEnqueueReadBuffer(
+		queue, 
+		outputSignalBuffer, 
+		CL_TRUE,
+        0, 
+		sizeof(cl_uint) * outputSignalHeight * outputSignalHeight, 
+		outputSignal,
+        0, 
+		NULL, 
+		NULL);
+	checkErr(errNum, "clEnqueueReadBuffer");
+    
+    auto convEndTime = std::chrono::system_clock::now();
+    std::chrono::duration<double> covTotalTime = convEndTime-convStartTime;
+
+    auto progEndTime = std::chrono::system_clock::now();
+    std::chrono::duration<double> progTotalTime = progEndTime-progStartTime;
+    std::cout << "Execution of entire program took: " << progTotalTime.count() << " seconds. Covolution time was " << covTotalTime.count() << " seconds." << std::endl;
+
+    // Output the result buffer
+    for (int y = 0; y < outputSignalHeight; y++)
+	{
+		for (int x = 0; x < outputSignalWidth; x++)
+		{
+			std::cout << outputSignal[y][x] << " ";
+		}
+		std::cout << std::endl;
 	}
+
+    std::cout << std::endl << "Executed program succesfully." << std::endl;
+
+	return 0;
 }
