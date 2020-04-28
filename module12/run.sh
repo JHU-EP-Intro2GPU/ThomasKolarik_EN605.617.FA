63ce8dd5bcce8c128bfc574219b508391e4f218f
Thomas Kolarik <tokolarik@gmail.com>
2020-04-28 16:43:41 -0400

    Use linux time to give some run time examples.

--- module12/run.sh
+++ module12/run.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 make
 echo "Testing execution with values 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16"
-./assignment.exe --values 16 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16
+time ./assignment.exe --values 16 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16
 echo "Testing execution with values 16 25 18 62 101 256 825 32 76 15 199 10003 12 8504 746758 635272 78594873"
-./assignment.exe --values 16 25 18 62 101 256 825 32 76 15 199 10003 12 8504 746758 635272 78594873
+time ./assignment.exe --values 16 25 18 62 101 256 825 32 76 15 199 10003 12 8504 746758 635272 78594873
