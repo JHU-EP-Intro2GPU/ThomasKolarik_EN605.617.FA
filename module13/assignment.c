97c7ea38d2db839bb8d98a5cfcf31ad6241df77b
Thomas Kolarik <tokolarik@gmail.com>
2020-05-07 20:42:18 -0400

    Fix some logic

--- module13/assignment.c
+++ module13/assignment.c
@@ -24,12 +24,12 @@
 #define NUM_BUFFER_ELEMENTS 16
 
 const std::unordered_set<std::string> validKernals = {
-	"add",
-	"sub",
-	"mult",
-	"div",
-	"mod",
-	"avg"	
+    "add",
+    "sub",
+    "mult",
+    "div",
+    "mod",
+    "avg"    
 };
 
 // Function to check and handle OpenCL errors
@@ -43,7 +43,7 @@ checkErr(cl_int err, const char * name)
 }
 
 ///
-//	main() for simple buffer and sub-buffer example
+//    main() for simple buffer and sub-buffer example
 //
 int main(int argc, char** argv)
 {
@@ -52,50 +52,58 @@ int main(int argc, char** argv)
     cl_uint numDevices;
     cl_platform_id * platformIDs;
     cl_device_id * deviceIDs;
-	 int platform = DEFAULT_PLATFORM; 
-	
-	// Make all of these vector in order to handle any number of input commands
+    int platform = DEFAULT_PLATFORM; 
+    
+    // Make all of these vector in order to handle any number of input commands
     std::vector<cl_context> contexts;
     std::vector<cl_program> programs;
-    std::vector<int *> inputOutputs;	
-	std::vector<std::string> kernalNames;
-	std::vector<cl_mem> buffers;
-	std::vector<cl_command_queue> queues;
-	std::vector<cl_kernel> kernals;
-	std::vector<cl_event> events;
-	
-	// Always start at 1 since argument 0 is the program name
-	for (int i = 1; i < argc; ++i)
-	{
-		
-		std::string kernalName(argv[i]);
-		
-		std::transform(kernalName.begin(), kernalName.end(), kernalName.begin(), ::tolower);
-		
-		if (validKernals.find(kernalName) == validKernals.end())
-		{
-			std::cout << kernalName << " is not a valid kernal name!" << std::endl;
-		}
-		
-		kernalNames.push_back(kernalName);
-	}
-	
-	if (kernalNames.size() == 0)
-	{
-		std::cout << "No valid kernals were supplied. Exiting!" << std::endl;
-		return 1;
-	}
-	
-	// Need to resize these. We could try to push_back but that would cause issues
-	// since everything is passed as pointers.
-	contexts.resize(kernalNames.size());
-	programs.resize(kernalNames.size());
-	inputOutputs.resize(kernalNames.size());
-	buffers.resize(kernalNames.size());
-	queues.resize(kernalNames.size());
-	kernals.resize(kernalNames.size());
-	events.resize(kernalNames.size());
-	
+    std::vector<float *> inputs0;    
+    std::vector<float *> inputs1;   
+    std::vector<float *> outputs;    
+    std::vector<std::string> kernalNames;
+    std::vector<cl_mem> buffers0;
+    std::vector<cl_mem> buffers1;
+    std::vector<cl_mem> outputBuffer;
+    std::vector<cl_command_queue> queues;
+    std::vector<cl_kernel> kernals;
+    std::vector<cl_event> events;
+    
+    // Always start at 1 since argument 0 is the program name
+    for (int i = 1; i < argc; ++i)
+    {
+        
+        std::string kernalName(argv[i]);
+        
+        std::transform(kernalName.begin(), kernalName.end(), kernalName.begin(), ::tolower);
+        
+        if (validKernals.find(kernalName) == validKernals.end())
+        {
+            std::cout << kernalName << " is not a valid kernal name!" << std::endl;
+        }
+        
+        kernalNames.push_back(kernalName);
+    }
+    
+    if (kernalNames.size() == 0)
+    {
+        std::cout << "No valid kernals were supplied. Exiting!" << std::endl;
+        return 1;
+    }
+    
+    // Need to resize these. We could try to push_back but that would cause issues
+    // since everything is passed as pointers.
+    contexts.resize(kernalNames.size());
+    programs.resize(kernalNames.size());
+    inputs0.resize(kernalNames.size());
+    inputs1.resize(kernalNames.size());
+    outputs.resize(kernalNames.sieze());
+    buffers0.resize(kernalNames.size());
+    buffers1.resize(kernalNames.size());
+    outputBuffer.resize(kernalNames.size());
+    queues.resize(kernalNames.size());
+    kernals.resize(kernalNames.size());
+    events.resize(kernalNames.size());
+    
     // First, select an OpenCL platform to run on.  
     errNum = clGetPlatformIDs(0, NULL, &numPlatforms);
     checkErr( 
@@ -154,144 +162,181 @@ int main(int argc, char** argv)
         (cl_context_properties)platformIDs[platform],
         0
     };
-	
-	for (size_t i = 0; i < kernalNames.size(); ++i)
-	{
-		const auto & kernalName = kernalNames[i];
-	    contexts[i] = clCreateContext(
-			contextProperties, 
-			numDevices,
-			deviceIDs, 
-			NULL,
-			NULL, 
-			&errNum);
-		checkErr(errNum, "clCreateContext");
-	 
-		// Create program from source
-		programs[i] = clCreateProgramWithSource(
-			contexts[i], 
-			1,
-			&src, 
-			&length, 
-			&errNum);
-		checkErr(errNum, "clCreateProgramWithSource");
-	 
-		// Build program
-		errNum = clBuildProgram(
-			programs[i],
-			numDevices,
-			deviceIDs,
-			"-I.",
-			NULL,
-			NULL);
-	 
-		if (errNum != CL_SUCCESS) 
-		{
-			// Determine the reason for the error
-			char buildLog0[16384];
-			clGetProgramBuildInfo(
-				programs[i], 
-				deviceIDs[0], 
-				CL_PROGRAM_BUILD_LOG,
-				sizeof(buildLog0), 
-				buildLog0, 
-				NULL);
+    
+    for (size_t i = 0; i < kernalNames.size(); ++i)
+    {
+        const auto & kernalName = kernalNames[i];
+        contexts[i] = clCreateContext(
+            contextProperties, 
+            numDevices,
+            deviceIDs, 
+            NULL,
+            NULL, 
+            &errNum);
+        checkErr(errNum, "clCreateContext");
+     
+        // Create program from source
+        programs[i] = clCreateProgramWithSource(
+            contexts[i], 
+            1,
+            &src, 
+            &length, 
+            &errNum);
+        checkErr(errNum, "clCreateProgramWithSource");
+     
+        // Build program
+        errNum = clBuildProgram(
+            programs[i],
+            numDevices,
+            deviceIDs,
+            "-I.",
+            NULL,
+            NULL);
+     
+        if (errNum != CL_SUCCESS) 
+        {
+            // Determine the reason for the error
+            char buildLog0[16384];
+            clGetProgramBuildInfo(
+                programs[i], 
+                deviceIDs[0], 
+                CL_PROGRAM_BUILD_LOG,
+                sizeof(buildLog0), 
+                buildLog0, 
+                NULL);
 
-				std::cerr << "Error in OpenCL C source: " << std::endl;
-				std::cerr << buildLog0;
-				checkErr(errNum, "clBuildProgram");
-		}
-		
-		// create buffers and sub-buffers
-		inputOutputs[i] = new int[NUM_BUFFER_ELEMENTS * numDevices];
-		for (unsigned int i = 0; i < NUM_BUFFER_ELEMENTS * numDevices; i++)
-		{
-			inputOutputs[i][i] = i;
-		}
+                std::cerr << "Error in OpenCL C source: " << std::endl;
+                std::cerr << buildLog0;
+                checkErr(errNum, "clBuildProgram");
+        }
+        
+        // create input output buffers0
+        inputs0[i] = new int[NUM_BUFFER_ELEMENTS * numDevices];
+        for (unsigned int j = 0; j < NUM_BUFFER_ELEMENTS * numDevices; ++j)
+        {
+            inputs0[i][j] = j;
+        }
+        
+        inputs1[i] = new int[NUM_BUFFER_ELEMENTS * numDevices];
+        for (unsigned int j = 0; j < NUM_BUFFER_ELEMENTS * numDevices; ++j)
+        {
+            inputs1[i][j] = 2 * j + i;
+        }
 
-		// create a single buffer to cover all the input data
-		buffers[i] = clCreateBuffer(
-			contexts[i],
-			CL_MEM_READ_WRITE,
-			sizeof(int) * NUM_BUFFER_ELEMENTS * numDevices,
-			NULL,
-			&errNum);
-		checkErr(errNum, "clCreateBuffer");
+        // create a single buffer to cover all the input data
+        buffers0[i] = clCreateBuffer(
+            contexts[i],
+            CL_MEM_READ_WRITE,
+            sizeof(int) * NUM_BUFFER_ELEMENTS * numDevices,
+            NULL,
+            &errNum);
+        checkErr(errNum, "clCreateBuffer");
+        
+        // create a single buffer to cover all the input data
+        buffers1[i] = clCreateBuffer(
+            contexts[i],
+            CL_MEM_READ_WRITE,
+            sizeof(int) * NUM_BUFFER_ELEMENTS * numDevices,
+            NULL,
+            &errNum);
+        checkErr(errNum, "clCreateBuffer");
+        
+        // create a single buffer to cover all the input data
+        outputBuffer[i] = clCreateBuffer(
+            contexts[i],
+            CL_MEM_READ_WRITE,
+            sizeof(int) * NUM_BUFFER_ELEMENTS * numDevices,
+            NULL,
+            &errNum);
+        checkErr(errNum, "clCreateBuffer");
 
-		// Create command queues
-		InfoDevice<cl_device_type>::display(
-			deviceIDs[0], 
-			CL_DEVICE_TYPE, 
-			"CL_DEVICE_TYPE");
+        // Create command queues
+        InfoDevice<cl_device_type>::display(
+            deviceIDs[0], 
+            CL_DEVICE_TYPE, 
+            "CL_DEVICE_TYPE");
 
-		 queues[i] =  
-			clCreateCommandQueue(
-			contexts[i],
-			deviceIDs[0],
-			0,
-			&errNum);
-		checkErr(errNum, "clCreateCommandQueue");
-	 
-		kernals[i] = clCreateKernel(
-		 programs[i],
-		 kernalName.c_str(),
-		 &errNum);
-		checkErr(errNum, ("clCreateKernel(" + kernalName + ")").c_str());
+         queues[i] =  
+            clCreateCommandQueue(
+            contexts[i],
+            deviceIDs[0],
+            0,
+            &errNum);
+        checkErr(errNum, "clCreateCommandQueue");
+     
+        kernals[i] = clCreateKernel(
+         programs[i],
+         kernalName.c_str(),
+         &errNum);
+        checkErr(errNum, ("clCreateKernel(" + kernalName + ")").c_str());
 
-		errNum = clSetKernelArg(kernals[i], 0, sizeof(cl_mem), (void *)&buffers[i]);
-		checkErr(errNum, ("clSetKernelArg(" + kernalName + ")").c_str());
-	 
-		// Write input data
-		errNum = clEnqueueWriteBuffer(
-		  queues[i],
-		  buffers[i],
-		  CL_TRUE,
-		  0,
-		  sizeof(int) * NUM_BUFFER_ELEMENTS * numDevices,
-		  (void*)inputOutputs[i],
-		  0,
-		  NULL,
-		  NULL);
+        errNum = clSetKernelArg(kernals[i], 0, sizeof(cl_mem), (void *)&buffers0[i]);
+        errNum = clSetKernelArg(kernals[i], 1, sizeof(cl_mem), (void *)&buffers1[i]);
+        checkErr(errNum, ("clSetKernelArg(" + kernalName + ")").c_str());
+     
+        // Write input data
+        errNum = clEnqueueWriteBuffer(
+          queues[i],
+          buffers0[i],
+          CL_TRUE,
+          0,
+          sizeof(int) * NUM_BUFFER_ELEMENTS * numDevices,
+          (void*)inputs0[i],
+          0,
+          NULL,
+          NULL);
+          
+        // Write input data
+        errNum = clEnqueueWriteBuffer(
+          queues[i],
+          buffers1[i],
+          CL_TRUE,
+          0,
+          sizeof(int) * NUM_BUFFER_ELEMENTS * numDevices,
+          (void*)inputs1[i],
+          0,
+          NULL,
+          NULL);
 
-		size_t gWI = NUM_BUFFER_ELEMENTS;
+        size_t gWI = NUM_BUFFER_ELEMENTS;
 
-		errNum = clEnqueueNDRangeKernel(
-		  queues[i], 
-		  kernals[i], 
-		  1, 
-		  NULL,
-		  (const size_t*)&gWI, 
-		  (const size_t*)NULL, 
-		  0, 
-		  0, 
-		  &events[i]);
-	}	  
-	
- 	//Wait for the last queue to complete before continuing on queue 0
- 	errNum = clEnqueueBarrier(queues[0]);
- 	errNum = clEnqueueWaitForEvents(queues[0], 1, &events.back());
-	
-	for (size_t i = 0; i < kernalNames.size(); ++i)
-	{
-		// Read back computed data
-		clEnqueueReadBuffer(
-				queues[i],
-				buffers[i],
-				CL_TRUE,
-				0,
-				sizeof(int) * NUM_BUFFER_ELEMENTS * numDevices,
-				(void*)inputOutputs[i],
-				0,
-				NULL,
-				NULL);
-	 
-		// Display output in rows
-		for (unsigned elems = 0; elems < NUM_BUFFER_ELEMENTS; elems++)
-		{
-		 std::cout << " " << inputOutputs[i][elems];
-		}
-		std::cout << std::endl;
-	}
+        errNum = clEnqueueNDRangeKernel(
+          queues[i], 
+          kernals[i], 
+          1, 
+          NULL,
+          (const size_t*)&gWI, 
+          (const size_t*)NULL, 
+          0, 
+          0, 
+          &events[i]);
+    }      
+    
+     //Wait for the last queue to complete before continuing on queue 0
+     errNum = clEnqueueBarrier(queues[0]);
+     errNum = clEnqueueWaitForEvents(queues[0], 1, &events.back());
+    
+    for (size_t i = 0; i < kernalNames.size(); ++i)
+    {
+        // Read back computed data
+        clEnqueueReadBuffer(
+                queues[i],
+                outputBuffer[i],
+                CL_TRUE,
+                0,
+                sizeof(int) * NUM_BUFFER_ELEMENTS * numDevices,
+                (void*)outputs[i],
+                0,
+                NULL,
+                NULL);
+     
+        // Display output in rows
+        for (unsigned elems = 0; elems < NUM_BUFFER_ELEMENTS; elems++)
+        {
+         std::cout << " " << outputs[i][elems];
+        }
+        std::cout << std::endl;
+    }
  
     std::cout << "Program completed successfully" << std::endl;
 
