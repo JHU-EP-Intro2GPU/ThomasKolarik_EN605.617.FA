5788ad162627809150173cdf7373ebb7c7a20889
Thomas Kolarik <tokolarik@gmail.com>
2020-05-10 18:38:10 -0400

    Convert everything to reading/writing out a PGM file.

--- Final Project/assignment.cu
+++ Final Project/assignment.cu
@@ -11,6 +11,81 @@
 #include <ImagesNPP.h>
 #include <ImageIO.h>
 
+const int WHITE_PIXEL = 256;
+const int BLACK_PIXEL = 0;
+const int ALIVE = 1;
+const int DEAD = 0;
+
+// Prints the given array out to the console
+// array: The array with the game world stored within it
+// xSize: The size of the array in the X direction
+// ySize: The size of the array in the Y direction
+// neighborsToGrow: The number of neighbors required for a cell
+void printArray(const unsigned int * const array, const unsigned int xSize, const unsigned int ySize)
+{
+    for (size_t y = 0; y < ySize; ++y)
+    {
+        for(size_t x = 0; x < xSize; ++x)
+        {
+            std::cout << array[y * xSize + x] << " ";
+        }
+        std::cout << '\n';
+    }
+    
+    std::cout << std::flush;
+}
+
+// Reads a given PGM file and populates the given array, as well as the x and y sizes
+// pgmName: The name of the PGM file to read.
+// array: The array to populate with data.
+// xSize: The xSize of the PGM.
+// ySize: The ySize of the PGM.
+void readPGM(const std::string & pgmName, unsigned int * array, unsigned int & xSize, unsigned int & ySize)
+{
+    // declare a host image object for an 8-bit grayscale image
+    npp::ImageCPU_8u_C1 oHostSrc;
+    // load gray-scale image from disk
+    npp::loadImage(pgmName, oHostSrc);
+    
+    unsigned int xSize = oHostSrc.size().nWidth;
+    unsigned int ySize = oHostSrc.size().nHeight;
+    
+    array = (unsigned int*)calloc(xSize * ySize, sizeof(unsigned int));
+    
+    // Put all of the image data into the given array.
+    for (int y = 0; y < ySize; ++y)
+    {
+        for (int x = 0; x < xSize; ++x)
+        {
+            array[y * ySize + j] = oHostSrc.data()[y * xSize + x] == WHITE_PIXEL ? DEAD : ALIVE;
+        }
+    }
+}
+
+// Writes a PGM file with the given name.
+// pgmName: The name of the PGM file to write.
+// array: The array to pull data from.
+// xSize: The xSize of the array.
+// ySize: The ySize of the array.
+void writePGM(const std::string & pgmName, const unsigned int * array, const unsigned int xSize, const unsigned int ySize)
+{
+    
+    npp::ImageCPU_8u_C1 oHostDst;
+    
+    oHostDst.size().setWidth(xSize);
+    oHostDst.size().setHeight(ySize);
+
+    // Put all of the image data into the given array.
+    for (int y = 0; y < ySize; ++y)
+    {
+        for (int x = 0; x < xSize; ++x)
+        {
+            // PGM white pixels (256) are dead and black pixels (0) are alive.
+            oHostSrc.data()[y * xSize + x] = array[y * ySize + j] != DEAD ? BLACK_PIXEL : WHITE_PIXEL;
+        }
+    }
+}
+
 // Counts the number of alive neighbors for a given square
 // array: The array with the game world stored within it
 // xSize: The size of the game world in the X direction
@@ -35,14 +110,14 @@ __device__ unsigned int countAliveNeighbors(const unsigned int * array, const un
     
     // We unravel the obvious set of loops here to have to skip checking the
     // if condition of the neighbor equaling the current square.
-    aliveNeighbors += (array[y0 * xSize + x0] != 0);
-    aliveNeighbors += (array[y1 * xSize + x0] != 0);
-    aliveNeighbors += (array[y2 * xSize + x0] != 0);
-    aliveNeighbors += (array[y0 * xSize + x1] != 0);
-    aliveNeighbors += (array[y2 * xSize + x1] != 0);
-    aliveNeighbors += (array[y0 * xSize + x2] != 0);
-    aliveNeighbors += (array[y1 * xSize + x2] != 0);
-    aliveNeighbors += (array[y2 * xSize + x2] != 0);
+    aliveNeighbors += (array[y0 * xSize + x0] != DEAD);
+    aliveNeighbors += (array[y1 * xSize + x0] != DEAD);
+    aliveNeighbors += (array[y2 * xSize + x0] != DEAD);
+    aliveNeighbors += (array[y0 * xSize + x1] != DEAD);
+    aliveNeighbors += (array[y2 * xSize + x1] != DEAD);
+    aliveNeighbors += (array[y0 * xSize + x2] != DEAD);
+    aliveNeighbors += (array[y1 * xSize + x2] != DEAD);
+    aliveNeighbors += (array[y2 * xSize + x2] != DEAD);
     
     return aliveNeighbors;
 }
@@ -93,25 +168,6 @@ void executeDevice(const unsigned int * array, const unsigned int xSize, const u
     std::cout << "Device execution took: " << totalTime.count() << " seconds." << std::endl;
 }
 
-// Prints the given array out to the console
-// array: The array with the game world stored within it
-// xSize: The size of the array in the X direction
-// ySize: The size of the array in the Y direction
-// neighborsToGrow: The number of neighbors required for a cell
-void printArray(const unsigned int * const array, const unsigned int xSize, const unsigned int ySize)
-{
-    for (size_t y = 0; y < ySize; ++y)
-    {
-        for(size_t x = 0; x < xSize; ++x)
-        {
-            std::cout << array[y * xSize + x] << " ";
-        }
-        std::cout << '\n';
-    }
-    
-    std::cout << std::flush;
-}
-
 // Counts the number of alive neighbors for a given square
 // array: The array with the game world stored within it
 // xSize: The size of the game world in the X direction
@@ -136,14 +192,14 @@ unsigned int hostCountAliveNeighbors(const unsigned int * array, const unsigned 
     
     // We unravel the obvious set of loops here to have to skip checking the
     // if condition of the neighbor equaling the current square.
-    aliveNeighbors += (array[y0 * xSize + x0] != 0);
-    aliveNeighbors += (array[y1 * xSize + x0] != 0);
-    aliveNeighbors += (array[y2 * xSize + x0] != 0);
-    aliveNeighbors += (array[y0 * xSize + x1] != 0);
-    aliveNeighbors += (array[y2 * xSize + x1] != 0);
-    aliveNeighbors += (array[y0 * xSize + x2] != 0);
-    aliveNeighbors += (array[y1 * xSize + x2] != 0);
-    aliveNeighbors += (array[y2 * xSize + x2] != 0);
+    aliveNeighbors += (array[y0 * xSize + x0] != DEAD);
+    aliveNeighbors += (array[y1 * xSize + x0] != DEAD);
+    aliveNeighbors += (array[y2 * xSize + x0] != DEAD);
+    aliveNeighbors += (array[y0 * xSize + x1] != DEAD);
+    aliveNeighbors += (array[y2 * xSize + x1] != DEAD);
+    aliveNeighbors += (array[y0 * xSize + x2] != DEAD);
+    aliveNeighbors += (array[y1 * xSize + x2] != DEAD);
+    aliveNeighbors += (array[y2 * xSize + x2] != DEAD);
     
     return aliveNeighbors;
 }
@@ -176,63 +232,64 @@ void hostProgressTime(const unsigned int * array, unsigned int * result, const u
 // ySize: The size of the array in the Y direction
 // neighborsToGrow: The number of neighbors required for a cell to grow if previously dead.
 // neighborsToDie: The number of neighbors at which the cell will die due to loneliness.
-void executeHost(const unsigned int * array, const unsigned int xSize, const unsigned int ySize, const unsigned int neighborsToGrow, const unsigned int neighborsToDie)
+// iterations: The number of game iterations to run.
+void executeHost(const unsigned int * array, const unsigned int xSize, const unsigned int ySize, const unsigned int neighborsToGrow, const unsigned int neighborsToDie, const unsigned int iterations)
 {
     auto startTime = std::chrono::system_clock::now();
-    unsigned int * result = (unsigned int*)calloc(xSize * ySize, sizeof(unsigned int));
-    hostProgressTime(array, result, xSize, ySize, neighborsToGrow, neighborsToDie);
+    unsigned int ** results;
+    results = calloc(iterations, sizeof(unsigned int *));
+    
+    for (unsigned int iter = 0; iter < iterations; ++iter)
+    {
+        results[iter] = calloc(xSize * ySize, sizeof(unsigned int));
+        hostProgressTime(array, results[iter], xSize, ySize, neighborsToGrow, neighborsToDie);
+        memcpy(array, results[iter], xSize * ySize * sizeof(unsigned int *));
+    }
     auto endTime = std::chrono::system_clock::now();
     std::chrono::duration<double> totalTime = endTime-startTime;
     std::cout << "Host execution took: " << totalTime.count() << " seconds." << std::endl;
     
-    printArray(array, xSize, ySize);
-    printArray(result, xSize, ySize);
+    // Output the data as a PGM and then free up the memory.
+    for (unsigned int iter = 0; iter < iterations; ++iter)
+    {
+        writePGM("cpuIter" + std::to_string(iter), const unsigned int * array, const unsigned int xSize, const unsigned int ySize);
+        free(results[iter]);
+    }
+    
+    free(results);
 }
 
 // Takes in a file name and parses it to setup the initial game state.
-// argc: The number of command line arguments. User should only ever input a single argument
-// argv: Stores the command line arguments. The only user argument should be the file to read from.
+// argc: The number of command line arguments.
+// argv: Stores the command line arguments.
 int main(int argc, char** argv)
-{
-    // declare a host image object for an 8-bit grayscale image
-    npp::ImageCPU_8u_C1 oHostSrc;
-    // load gray-scale image from disk
-    npp::loadImage("Example.pgn", oHostSrc);
-    
-    std::cout << oHostSrc.size().nHeight << std::endl;
-    std::cout << oHostSrc.size().nWidth << std::endl;
-    
-    for (int i = 0; i < oHostSrc.size().nHeight; ++i)
+{ 
+    // Make sure we are inputing all of the correct arguments.
+    if (argc != 5)
     {
-        for (int j = 0; j < oHostSrc.size().nWidth; ++j)
-        {
-            std::cout << oHostSrc.data()[i * oHostSrc.size().nWidth + j] << " ";
-        }
-        std::cout << std::endl;
-    }
-    /*// Argc should only have a single argument which is the name of the file to read.
-    if (argc != 4)
-    {
-        std::cout << "Invalid number of arguments. Usage 'gameOfLife.exe # # Example.pgn' where the # are unsigned int for neighborsToGrow, and neighborsToDie respectively." << std::endl;
+        std::cout << "Invalid number of arguments. Usage 'gameOfLife.exe # # # Example.pgn' where the # are unsigned int for neighborsToGrow, neighborsToDie, and the number of iterations respectively." << std::endl;
         
         return -1;
     }
     
     const unsigned int GROW_INDEX = 1;
     const unsigned int DIE_INDEX  = 2;
-    const unsigned int PGN_INDEX  = 3;
+    const unsigned int ITER_INDEX = 3;
+    const unsigned int PGM_INDEX  = 4;
 
-    unsigned int xSize = 0;
-    unsigned int ySize = 0;
     unsigned int neighborsToGrow = std::stoul(argc[GROW_INDEX]);
     unsigned int neighborsToDie = std::stoul(argc[DIE_INDEX]);
-
-    unsigned int * array = (unsigned int*)calloc(xSize * ySize, sizeof(unsigned int));
-
-    executeHost(array, xSize, ySize, neighborsToGrow, neighborsToDie);
+    unsigned int iterations = std::stoul(argc[ITER_INDEX]);
     
+    unsigned int * array;
+    unsigned int xSize = 0;
+    unsigned int ySize = 0;
     
-    free(array);*/
+    readPGM(argc[PGM_INDEX], array, xSize, ySize);
+
+    executeHost(array, xSize, ySize, neighborsToGrow, neighborsToDie, iterations);
+    
+    free(array);
     
     return 0;
 }
