056c0b44e27600e677f8a637f9e0e7079ce0ab94
Thomas Kolarik <tokolarik@gmail.com>
2020-05-11 14:57:26 -0400

    Rename files.

--- Final Project/run.sh
+++ Final Project/run.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 make
 echo "Testing execution with Blank.pgm"                                                                                                                                                                                      
-./assignment.exe 3 1 1 Blank.pgm
+./GameOfLife.exe 3 1 1 Blank.pgm
 echo "Testing execution with Glider.pgm"                                                                                                                                                                                    
-./assignment.exe 3 1 1 Glider.pgm
+./GameOfLife.exe 3 1 1 Glider.pgm
 echo "Testing execution with 1920x1440.pgm"                                                                                                                                                                                    
-./assignment.exe 3 1 1 1920x1440.pgm
+./GameOfLife.exe 3 1 1 1920x1440.pgm
